ab1b864558d36a8513e7af674b9c125e33dc7229
torresposso <erictorres.co@gmail.com>
2023-06-27 09:53:38 -0500

    first commit

---
+++ routes/_app.tsx
@@ -0,0 +1,42 @@
+import { asset, Head } from "$fresh/runtime.ts";
+import { AppProps } from "$fresh/src/server/types.ts";
+
+export default function AppLayout({ Component }: AppProps) {
+  const presets = `
+  twind.install({
+    presets: [
+      twind.presetExt(),        
+      twind.presetTypography(),
+   ],
+  })
+  `;
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <title>Tombot - AI Agent</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <script
+          src="https://cdn.twind.style/ext,typography"
+          crossOrigin=""
+        />
+        <script>
+          {presets}
+        </script>
+        <link rel="stylesheet" href="https://unpkg.com/open-props" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/open-props/animations.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/open-props/gradients.min.css"
+        />
+      </Head>
+      <body class="[background-image:var(--gradient-8)] h-screen">
+        <Component />
+      </body>
+    </>
+  );
+}
---
+++ routes/index.tsx
@@ -0,0 +1,52 @@
+import { Head } from "$fresh/runtime.ts";
+import SquigglyLines from "@/components/SquigglyLines.tsx";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Cuartify</title>
+      </Head>
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20">
+        <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
+          Generating dream rooms{" "}
+          <span className="relative whitespace-nowrap text-blue-600">
+            <SquigglyLines />
+            <span className="relative">using AI</span>
+          </span>{" "}
+          for everyone.
+        </h1>
+        <h2 className="mx-auto mt-12 max-w-xl text-lg sm:text-gray-400  text-gray-500 leading-7">
+          Take a picture of your room and see how your room looks in different
+          themes. 100% free – remodel your room today.
+        </h2>
+        <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
+          <div className="flex flex-col space-y-10 mt-4 mb-16">
+            <div className="flex sm:space-x-8 sm:flex-row flex-col">
+              <div>
+                <h3 className="mb-1 font-medium text-lg">Original Room</h3>
+                <img
+                  alt="Original photo of a room with roomGPT.io"
+                  src="/original-pic.jpg"
+                  className="w-full object-cover h-96 rounded-2xl border"
+                  width={400}
+                  height={400}
+                />
+              </div>
+              <div className="sm:mt-0 mt-8">
+                <h3 className="mb-1 font-medium text-lg">Generated Room</h3>
+                <img
+                  alt="Generated photo of a room with roomGPT.io"
+                  width={400}
+                  height={400}
+                  src="/generated-pic-2.jpg"
+                  className="w-full object-cover h-96 rounded-2xl sm:mt-0 mt-2 border"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
